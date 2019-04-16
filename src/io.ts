15cb726232bd5d6947bee98bbe688eea6ab0ef29
Rubikscraft <renzodepenzo@live.nl>
2019-04-16 21:00:33 +0200

    expressions and subcommands added

--- src/io.ts
+++ src/io.ts
@@ -4,6 +4,9 @@ export class IO {
       case 'stdout':
         process.stdout.write(string);
         break;
+      case 'stderr':
+        process.stderr.write(string);
+        break;
       default:
         throw new Error(`can not find channel named "${channelId}"`);
     }
